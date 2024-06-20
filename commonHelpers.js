24f5981d848b38c39408f8a4baa68dae42b08911
SilviaZhdanska <silviaalbert1992@gmail.com>
2024-06-20 21:06:21 +0000

    Deploying to gh-pages from @ SilviaZhdanska/goit-js-hw-09@97407bf26c35f37b4dc255d1bc0c43284c2ca139 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{S as e}from"./assets/vendor-10cb7c31.js";let o=new e(".gallery a");o.on("show.simplelightbox",function(){});o.on("error.simplelightbox",function(l){console.log(l)});document.querySelector(".gallery");
+//# sourceMappingURL=commonHelpers.js.map
